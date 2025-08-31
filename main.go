7d4978f1a5cb7dfa3c4a97e47fc1c6eecfed990f
ForeverThinking <35239092+ForeverThinking@users.noreply.github.com>
2025-08-31 18:06:14 +0100

    feat: add initial table and sqlite connection

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ForeverThinking/xplane-world-tour-backend/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	db.InitDB()
+
 	server := gin.Default()
 
 	server.Run(":8080")
